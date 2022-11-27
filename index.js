606b22b61c75c7d03ea1a008873f1f5b49fd0f28
anshu0x <anshusharma6327@gmail.com>
2022-11-27 18:44:41 +0530

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const app = express();
-const schema = require('./Schemas')
-
-
+const schema = require("./Schemas");
+const cors = require( `cors` );
+app.use( cors() )
 app.use(
   "/graphql",
   graphqlHTTP({
